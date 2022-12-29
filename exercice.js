ae4235a60b4d051d0426744fedda87f487b750d7
Martin Memel <memelmegame@gmail.com>
2022-12-29 10:40:14 +0000

    first commit

---
+++ exercice.js
@@ -0,0 +1,76 @@
+// Exercice 1 : Simple If/Else Statement
+
+// Create 2 variables, x and y. Each of them should have a different numeric value.
+let x = 10;
+let y = 20;
+
+// Write an if/else statement that checks which number is bigger.
+if (x > y) {
+  console.log(x +" is the biggest number");
+} else {
+  console.log(y +" is the biggest number");
+}
+
+
+// Exercise 2 : Chihuahua
+
+// Create a variable called where it’s value is “Chihuahua”
+let newDog = "Chihuahua";
+
+// Check and display how many letters are in
+console.log("there are"+ newDog.length+" letters in newDog");
+
+// Display the variable in uppercase (no need to create new variables, just console.log twice).
+console.log(newDog.toUpperCase);
+
+// Display the variable in lowercase (no need to create new variables, just console.log twice).
+console.log(newDog.toLowerCase);
+
+// Check if the variable is equal to “Chihuahua”
+if (newDog === "Chihuahua") {
+  console.log("I love Chihuahuas, it's my favorite dog breed");
+} else {
+  console.log("I don't care, I prefer cats");
+}
+// Results
+console.log("there are"+ newDog.length+" letters in newDog");
+
+
+
+// Exercise 3: Even Or Odd
+
+// Prompt the user for a number and save it to a variable.
+let number = prompt("Enter a number:");
+// Check whether the variable is even or odd
+
+// If it is even, display: “x is an even number”. Where x is the actual number the user chose.
+if (number % 2 === 0) {
+  console.log(`${number} is an even number.`);
+
+// If it is odd, display: “x is an odd number”. Where x is the actual number the user chose.
+} else {
+  console.log(`${number} is an odd number.`);
+}
+
+
+// Exercise 4: Group Chat
+
+const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+// Using the array above, console.log the number of users that are connected to the group chat based on the following rules:
+
+// If there is no users (the users array is empty), console.log “no one is online”.
+if (users.length === 0) {
+  console.log("no one is online");
+
+// If there is 1 user, console.log “ is online”.
+} else if (users.length === 1) {
+  console.log(`${users[0]} is online`);
+
+// If there are 2 users, console.log “ and are online”
+} else if (users.length === 2) {
+  console.log(`${users[0]} and ${users[1]} are online`);
+
+// If there are more than 2 users, console.log the first two names in the users array and the number of additional users online.
+} else {
+  console.log(`${users[0]}, ${users[1]} and ${users.length - 2} more are online`);
+}
